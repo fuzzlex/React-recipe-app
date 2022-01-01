5f2be9289e62474d70815d2059166a8c1428a150
emre <emrky.2018@icloud.com>
2022-01-01 14:26:56 +0300

    fds

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useContext } from 'react'
+import React from 'react'
 import Header from '../../components/header/Header'
 import { useState  } from 'react';
 import { MainContainer, ImgDiv, HomeImg } from "./HomeStyle"
