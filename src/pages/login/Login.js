0dfd4a493da35c9a321eec0ad687d138b01df36a
emre <emrky.2018@icloud.com>
2022-01-02 07:57:21 +0300

    sd

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -46,7 +46,7 @@ const Login = () => {
         </Segment>
       </Form>
       <Message>
-        New to us? <a href='#'>Sign Up</a>
+        New to us? <a  onClick={() => navigate("/register")}>Sign Up</a>
       </Message>
     </Grid.Column>
   </Grid>
